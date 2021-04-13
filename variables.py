89eb43eb0f9069028768e8da5c1e2dbd96905285
ShabnamSaidova <82189237+ShabnamSaidova@users.noreply.github.com>
2021-04-12 20:49:21 -0400

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,107 @@
+# naming the file: avoid spaces, avoid upper cases, '_' can be used to separate words
+# new_variables.py (python), newVariables.java (java)
+
+# Variables:
+#naming : should not start with numbers, starts with lowercase, symbols and numbers and can be used
+# nameofthevariable = value , declaring and setting a value for the variable
+vname = "shabnam" # "Shabnam"
+num = 12
+status = True
+price = 45.789
+
+message = "Hello class, we are starting to learn python!!"
+print("Hello Again")
+print(vname)
+print("____________")
+vname = 'Shabnam'
+num = 9876
+print(vname, "***", num, status, price, message)
+message = 'This is the message for the exercise 2-1'
+print (message)
+message = 'new message for exercise 2-2'
+print (message)
+
+# CTRL + D - duplicates the line that your cursor is on
+# CTRL + C - copies the line that your cursor is on, you don't have to highlight
+# CTRL + V -  pastes the copied content to the next line
+# SHIFT +ALT + UP/DOWN - takes  the line that your cursor is on UP/DOWN.
+
+
+message = 'Hello Python World!'
+print (message)
+message = 'Hello Python Crush Course!'
+print(message)
+mesage = "Hello Python Crash Course reader!"
+print(mesage)
+
+message = 'Good evening, we are starting practicing Python'
+print(message)
+print(vname)
+print('+++++++++')
+vname = 'Shani'
+num = '050577'
+print(vname, "***", num, status, price, message)
+
+name = 'ada lovelace'
+print(name.title())
+
+name = "Ada Lovelace"
+print(name.upper())
+print(name.lower())
+first_name = "ada"
+last_name = "lovelace"
+full_name = first_name + " " + last_name
+print(full_name)
+first_name = "ada"
+last_name = "lovelace"
+full_name = first_name + " " + last_name
+print("Hello, "  + full_name.title() + "!")
+message = ("Hello, " + full_name.title() + "!")
+print(message)
+print ('Python')
+print("\tPython")
+
+favorite_language = 'python '
+print(favorite_language)
+favorite_language.rstrip()
+favorite_language = ' python '
+favorite_language = favorite_language.rstrip()
+' python'
+favorite_language = favorite_language.lstrip()
+'python '
+favorite_language = favorite_language.strip()
+'python'
+message = "One of Python's strengths is its diverse community."
+print(message)
+
+price = 10
+rating = 4.9
+name = 'Shabnam'
+is_published = False
+print(price)
+
+name = 'John Smith'
+age = 20
+is_new =True
+name = 'Shabnam'
+name = input('What is your name? ')
+print('Hi ' + name)
+
+
+num1 = 486
+num2 = 680
+print(f"num1 + num2 = {num1 + num2}")
+print(f"num1 - num2 = {num1 - num2}")
+print(f"num1 * num2 = {num1 * num2}")
+
+quote = '\t\tAlbert Einstein once said, "A person who never made a mistake, never tried anything new.'
+print(quote)
+
+
+I am learning a new command
+
+
+
+
+
+
